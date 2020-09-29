82bb8dee2d408b99eda6ff1caead2018972a5eeb
Lars Bohm <larsbohm@gmail.com>
2020-09-29 13:09:16 +0200

    add lightbox with nav

---
+++ all.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const lightboxInputs = document.querySelectorAll(".lightbox-image input");
+  const lightboxNav = document.querySelector("#lightbox-nav");
+  const previousButton = document.querySelector("#previous");
+  const nextButton = document.querySelector("#next");
+  const buttons = [previousButton, nextButton];
+
+  const handleClick = (event) => {
+    const button = event.currentTarget;
+    const indexToActivate = button.dataset.target;
+    const input = lightboxInputs[indexToActivate];
+    input.checked = true;
+    handleNav(input);
+  };
+
+  const handleChange = (event) => {
+    const input = event.currentTarget;
+    handleNav(input);
+  };
+
+  const handleNav = (input) => {
+    if (!input.checked) {
+      lightboxNav.classList.add("collapsed");
+    } else {
+      lightboxNav.classList.remove("collapsed");
+    }
+
+    const currentIndex = [...lightboxInputs].findIndex(
+      (item) => item === input
+    );
+    const nextIndex =
+      currentIndex >= lightboxInputs.length - 1 ? 0 : currentIndex + 1;
+    const previousIndex =
+      currentIndex <= 0 ? lightboxInputs.length - 1 : currentIndex - 1;
+
+    previousButton.dataset.target = previousIndex;
+    nextButton.dataset.target = nextIndex;
+    [...lightboxInputs]
+      .filter((lightboxInput) => lightboxInput !== input)
+      .forEach((lightboxInput) => (lightboxInput.checked = false));
+  };
+
+  lightboxInputs.forEach((lightboxInput) => {
+    lightboxInput.addEventListener("change", handleChange);
+  });
+  buttons.forEach((button) => {
+    button.addEventListener("click", handleClick);
+  });
+});
